6572a4201cd15d9e672b22849848c4b4ce4f6edf
unknown <1090230661@qq.com>
2019-09-26 16:01:54 +0800

    完成分词的-d功能

--- src/task_4/WF.java
+++ src/task_4/WF.java
@@ -1,6 +1,7 @@
 package task_4;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,8 +15,9 @@ import java.util.TreeMap;
 public class WF {
 
 	/**
-	 * -c 输出某个英文文本文件中 26 字母出现的频率，由高到低排列，并显示字母出现的百分比，精确到小数点后面两位
-	 * 
+	 * -c <file> 输出某个英文文本文件中 26 字母出现的频率，由高到低排列，并显示字母出现的百分比，精确到小数点后面两位
+	 * -f <file> 输出文件中所有不重复的单词，按照出现次数由多到少排列，出现次数同样多的，以字典序排列
+	 * -d <directory> 指定文件目录，对目录下每一个文件执行  wf.exe -f <file> 的操作
 	 * @param args
 	 */
 	public static class Letter implements Comparable<Letter> {
@@ -64,30 +66,82 @@ public class WF {
 		}
 		
 	}
+	/**
+	 * 获取txt文件中的文本内容
+	 * @param fileName
+	 * @return
+	 */
+	public static String fileText( String fileName ){
+		BufferedReader in = null;
+		try {
+			in = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		String text = "" , line;
+        try {
+			while ( ( line = in.readLine() ) != null ) {
+				text += line;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        // text 为文件中的内容
+		return text;
+	}
+	
+	/**
+	 * 获取文件的后缀名
+	 * @param file
+	 * @return
+	 */
+	public static String getFileExtension(File file) {
+		String fileName = file.getName();
+		if(fileName.lastIndexOf(".") != -1)
+			return fileName.substring(fileName.lastIndexOf("."));
+		else {
+			return null;
+		}
+	}
+	
+	/**
+	 * 完成 -f 的功能
+	 * @param text
+	 */
+	public static void printF( String text ) {
+		text = text.replaceAll("[^0-9a-zA-Z]+", " ");
+		String words[] = text.split("\\s+");//分割一个或多个空格
+		Map< String , Integer > count = new TreeMap< String , Integer >();
+		for( String word : words ){
+			int cnt = 1;
+			if( count.get(word) != null ) cnt = count.get(word) + 1;
+			count.put( word , cnt );
+		}
+		
+        List<Map.Entry<String, Integer>> printWords = new ArrayList<Map.Entry<String, Integer>>(count.entrySet());
+        Collections.sort(printWords, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if(o1.getValue().compareTo(o2.getValue()) == 0){
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+        for( Map.Entry< String , Integer > word : printWords){ 
+        	System.out.println( word.getKey()+":" + word.getValue() ); 
+        }
+	}
 	public static void main(String[] args) {
 		if( args.length < 2 ){
 			System.out.println("命令不正确!");
 		}
 		else{
 			String option = args[0]; // 操作
-			String fileName = args[1]; // 文件名字
-			BufferedReader in = null;
-			try {
-				in = new BufferedReader(new FileReader(fileName));
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
-			String text = "" , line;
-	        try {
-				while ( ( line = in.readLine() ) != null ) {
-					text += line;
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-	        // text 为文件中的内容
 	        
 			if( option.equals("-c") ){
+				String fileName = args[1]; // 文件名字
+				String text = fileText( fileName );// text 为文件中的内容
 				int sum = 0; // 排除非字母
 				int cnt[] = new int[26];//统计每个字母出现的字数
 				for( int i = 0 ; i < text.length() ; i++ ){
@@ -114,29 +168,34 @@ public class WF {
 				}
 			}
 			else if( option.equals("-f") ){
-				text = text.replaceAll("[^0-9a-zA-Z]+", " ");
-				String words[] = text.split("\\s+");//分割一个或多个空格
-				Map< String , Integer > count = new TreeMap< String , Integer >();
-				for( String word : words ){
-					int cnt = 1;
-					if( count.get(word) != null ) cnt = count.get(word) + 1;
-					count.put( word , cnt );
-				}
-				
-		        List<Map.Entry<String, Integer>> printWords = new ArrayList<Map.Entry<String, Integer>>(count.entrySet());
-		        Collections.sort(printWords, new Comparator<Map.Entry<String, Integer>>() {
-		            @Override
-		            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-		                if(o1.getValue().compareTo(o2.getValue()) == 0){
-		                    return o1.getKey().compareTo(o2.getKey());
-		                }
-		                return o2.getValue().compareTo(o1.getValue());
-		            }
-		        });
-		        for( Map.Entry< String , Integer > word : printWords){ 
-		        	System.out.println( word.getKey()+":" + word.getValue() ); 
-		        }
+				String fileName = args[1]; // 文件名字
+				String text = fileText( fileName );// text 为文件中的内容
+		        printF(text);
 			}
+			else if( option.equals("-d") && !args[1].equals("-s") ){
+				String directoryPath = args[1];
+				File file = new File(directoryPath);//获取其file对象
+				File[] files = file.listFiles();//遍历path下的文件和目录，放在File数组中
+				for( File f : files ){//遍历File[]数组
+					if(!f.isDirectory() && getFileExtension(f).equals(".txt") ){ // 后缀名为 txt 文件
+						String text = fileText(directoryPath + f.getName());
+						System.out.println(f.getName() + "的单词统计功能如下:");
+						printF(text);
+					}
+				}
+			}
+			else if( option.equals("-d") && args[1].equals("-s") ){
+				String directoryPath = args[2];
+				
+				
+				
+			}
+			else if( option.equals("-d") ){
+				
+			}			
+			else if( option.equals("-d") ){
+				
+			}			
 			else if( option.equals("-d") ){
 				
 			}
